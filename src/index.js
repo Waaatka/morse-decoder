335bc29df58ee17be1c460fc9563b2a2fd496669
Anna Sapegina <watochka@gmail.com>
2021-08-23 19:52:03 +0300

    feat: implement morse decoder

--- src/index.js
+++ src/index.js
@@ -37,8 +37,28 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const splitByChunks = (str, chunkSize) => {
+    const result = []
+    for (let i = 0; i < str.length; i+= chunkSize) {
+        result.push(str.slice(i, i + chunkSize))
+    }
+    return result
+}
+
 function decode(expr) {
-    // write your solution here
+    return splitByChunks(expr, 10).map(x => {
+        if (x === '**********') {
+            return ' '
+        }
+        const key = splitByChunks(x, 2).map(y => {
+            switch (y) {
+                case '10': return '.'
+                case '11': return '-'
+                default: return ''
+            }
+        }).join('');
+        return MORSE_TABLE[key]
+    }).join('')
 }
 
 module.exports = {
